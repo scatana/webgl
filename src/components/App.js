275921061d560203431388b5d67889ccb1ed14f2
Sorin Cătană <scatana@users.noreply.github.com>
2020-03-13 21:48:46 -0400

    Streamline home screen

--- src/components/App.js
+++ src/components/App.js
@@ -6,42 +6,46 @@ import {
   Link
 } from "react-router-dom";
 
+import styles from './App.css';
+
 // Pull common components into the main chunk
 import LoadingIndicator from 'components/common/LoadingIndicator';
 import GLCanvas from 'components/common/GLCanvas';
 
 // Pages
 const HelloPoint1 = lazy(() => import(
-  /* webpackChunkName: "HelloPoint1" */
+  /* webpackChunkName: "p_HelloPoint1" */
   'components/pages/HelloPoint1'
 ));
 const ClearCanvas = lazy(() => import(
-  /* webpackChunkName: "ClearCanvas" */
+  /* webpackChunkName: "p_ClearCanvas" */
   'components/pages/ClearCanvas'
-));
-const Home = lazy(() => import(
-  /* webpackChunkName: "Home" */
-  'components/pages/Home'
 ));
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/hello-point-1">
-          <Suspense fallback={<LoadingIndicator />}>
-            <HelloPoint1 />
-          </Suspense>
-        </Route>
         <Route path="/clear-canvas">
           <Suspense fallback={<LoadingIndicator />}>
             <ClearCanvas />
           </Suspense>
         </Route>
-        <Route path="/">
+        <Route path="/hello-point-1">
           <Suspense fallback={<LoadingIndicator />}>
-            <Home />
+            <HelloPoint1 />
           </Suspense>
+        </Route>
+        <Route path="/">
+          <div className={styles.container}>
+            <h1 className={styles.pageTitle}>WebGL</h1>
+            <Link to="/hello-point-1" className={styles.card}>
+              Hello Point (1)
+            </Link>
+            <Link to="/clear-canvas" className={styles.card}>
+              Clear canvas
+            </Link>
+          </div>
         </Route>
       </Switch>
     </Router>
